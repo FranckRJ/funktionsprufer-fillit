7e69c4ced602951518618cbc2bf4f47bb6ff6169
Franck W <franck.rj.w@gmail.com>
2018-12-10 12:22:40 +0100

    Ajout de plus de tests avances.

--- fillitAdvancedErrorTest.cpp
+++ fillitAdvancedErrorTest.cpp
@@ -24,7 +24,7 @@ void fillitAdvancedErrorTest::processTest()
 			(void)fn;
 			return mkSpCppStrVal("Stdout :\nerror\n");
 		};
-	for (int i = 1; i <= 5; ++i)
+	for (int i = 1; i <= 8; ++i)
 	{
 		std::string numVal = (i < 10 ? "0" : "") + std::to_string(i);
 		testThisFun(baseFunction, testFunction, mkSpCstStrVal(std::string("advanced-error-test-" + numVal + ".txt").c_str()));
